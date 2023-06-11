a96fb6d014d797561d262bba55f684828dfab679
Gabriel  Mora <gabi@iMac-de-Gabriel.local>
2023-06-11 17:42:49 +0200

    Scripts de entrenamiento y renderizado del entorno añadidos

---
+++ scripts/ant_render.py
@@ -0,0 +1,20 @@
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+import os
+from my_ant import My_AntEnv
+
+models_dir = "models/PPO"
+model_path = f"{models_dir}/2490000.zip"
+
+TIMESTEPS = 100000
+env = make_vec_env('GoOne-v1')
+model = PPO.load(model_path, env=env)
+
+obs = env.reset()
+while True:
+    env.render()
+    action, _ = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+env.close()
---
+++ scripts/ant_train.py
@@ -0,0 +1,23 @@
+import gym
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+
+import os
+
+models_dir = "models/PPO"
+logdir = "logs"
+
+if not os.path.exists(models_dir):
+  os.makedirs(models_dir)
+
+if not os.path.exists(logdir):
+  os.makedirs(logdir)
+
+#TIMESTEPS = 100000
+TIMESTEPS = 10000
+env = make_vec_env('GoOne-v1', n_envs=1)
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
+
+for i in range(1, 1250):
+  model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
+  model.save(f"{models_dir}/{TIMESTEPS*i}")
